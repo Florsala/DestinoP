2f28bede99db3049d9d15319fc2f413a3bdae011
Flor Salamanca <florsala@gmail.com>
2022-12-08 20:48:44 -0300

    gris created in gallery page

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import '../styles/Galeria.css'
+import React from "react";
+import "../styles/Galeria.css";
+import "../styles/Medias.css";
 
 import monta from "../assets/monta.png";
-import ushuaia from '../assets/ush-am.jpg';
+import ushuaia from "../assets/ush-am.jpg";
+import harberton from "../assets/harberton.jpg";
+import lobos from "../assets/lobos.jpg";
+import ush from "../assets/ush-am.jpg";
+import paisaje from "../assets/paisaje.jpg";
 
 
+
+import { Col, Container, Row } from "react-bootstrap";
+
+import pinguinos from "../assets/pinguinos.jpg";
 
 const Galeria = () => {
   return (
     <div>
-
-<div>
+      <div>
         <img
           className="headerImg"
           style={{ filter: "brightness(0.9)" }}
@@ -20,20 +28,69 @@ const Galeria = () => {
         <img className="headerImg2" src={monta} alt="mount" />
 
         <div className="headerTitle_galeria  container-md">
-          <h2 className="text-uppercase display-2" style={{fontWeight:'600'}}>
-           Ushuaia 
-            
+          <h2
+            className="text-uppercase display-2"
+            style={{ fontWeight: "600" }}
+          >
+            Ushuaia
           </h2>
           <h4>en fotos</h4>
         </div>
       </div>
+
+      <div className="galeria_title">
+        <h2>Galería</h2>
+      </div>
+
+      <Container className="grid_galeria">
+        <img
+          style={{ width: "100%", height: "100%" }}
+          className="img1"
+          src={pinguinos}
+          alt=""
+          
+        />
+        <img className="img2" src={lobos} alt="" />
+        <img className="img3" src={harberton} alt="" />
+        <img className="img4" src={ush} alt="" />
+        <img className="img5" src={pinguinos} alt="" />
+
+       
+        <img className="img6" src={lobos} alt="" />
+        <img className="img7" src={harberton} alt="" />
+        <img
+          style={{ width: "100%", height: "100%" }}
+          className="img8"
+          src={paisaje}
+          alt=""
+        />
+        <img className="img9" src={ush} alt="" />
+        <img className="img10" src={pinguinos} alt="" />
+      </Container>
+
+
+
+
+    {/*   <Container>
+      <Row>
+        <Col xs={12} md={8}>
+        <img style={{width:'25rem', height:'15.625rem', objectFit:'cover'}} src={pinguinos} alt="" /></Col>
+      </Row>
+      <Row>
+      <Col><img style={{width:'12.5rem', height:'15.625rem', objectFit:'cover'}} src={pinguinos} alt="" /></Col>
+
+        <Col><img style={{width:'12.5rem', height:'15.625rem', objectFit:'cover'}} src={pinguinos} alt="" /></Col>
+        <Col><img style={{width:'12.5rem', height:'15.625rem', objectFit:'cover'}} src={pinguinos} alt="" /></Col>
+        <Col><img style={{width:'12.5rem', height:'15.625rem', objectFit:'cover'}} src={pinguinos} alt="" /></Col>
+      </Row>
+    </Container> */}
 
 
 
 
 
     </div>
-  )
-}
+  );
+};
 
-export default Galeria
+export default Galeria;
