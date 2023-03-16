cf636d57078501638c0d3c842a0dfd01a08e1576
Flor Salamanca <florsala@gmail.com>
2023-03-16 12:01:59 -0300

    prueba en nuevo menu

--- src/components/Menu/Navmenu.jsx
+++ src/components/Menu/Navmenu.jsx
@@ -9,11 +9,10 @@ import CartWidget from "./CartWidget";
 import cartContext from "../../context/CartContext";
 //import getMenu from "../helpers/getIdiomas";
 
-
 const Navmenu2 = () => {
   const [click, setClick] = useState(false);
   const [etiquetas, setEtiquetas] = useState([]);
-  const { idioma, getIdiomaSeccion } = useContext(cartContext)
+  const { idioma, getIdiomaSeccion } = useContext(cartContext);
 
   const [open, setOpen] = useState(false);
 
@@ -22,14 +21,12 @@ const Navmenu2 = () => {
   useEffect(() => {
     let handler = () => {
       setClick(false);
-    
     };
-    document.addEventListener("mousedown", handler)
-  })
-  useEffect(()=>{
-    setEtiquetas(getIdiomaSeccion('Menu'))
- },[idioma])
-
+    document.addEventListener("mousedown", handler);
+  });
+  useEffect(() => {
+    setEtiquetas(getIdiomaSeccion("Menu"));
+  }, [idioma]);
 
   const [color, setColor] = useState(false);
   const changeColor = () => {
@@ -44,132 +41,47 @@ const Navmenu2 = () => {
 
   return (
     <>
-{/*       <Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="light"
-        variant="light"
-        style={{ padding: " 0 2rem" }}
-        className={color ? "navbar navbar-bcg" : "navbar"}
-      >
-     
-        <div className="container-fluid ">
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse
-            id="responsive-navbar-nav"
-            style={{ justifyContent: "center" }}
-          >
-            <Nav className={color ? "navLink font-sm" : "navLink"}>
-              <Nav.Link as={NavLink} to={"/"} onClick={closeMenu}>
-              {etiquetas[0]?.palabra}
-              </Nav.Link>
-              <Nav.Link as={NavLink} to={"/servicios"} onClick={()=> setClick(false)}>
-              {etiquetas[1]?.palabra}
-              </Nav.Link>
-              <Nav.Link as={NavLink} to={"/blog"}>
-              {etiquetas[2]?.palabra}
-              </Nav.Link>
-
-
-<Nav.Link as={NavLink} to={"/"} style={{
-            margin: "0.3125rem auto",
-            display: "flex",
-            justifyContent: "center",
-            border:'none'
-          }}>
-          
-        
-            <img
-              src={logo}
-              alt="logo"
-              className={color ? "logo h-4" : "logo"}
-            />
-
-
-
-</Nav.Link>
-
-              <Nav.Link as={NavLink} to={"/nosotros"}>
-              {etiquetas[3]?.palabra}
-              </Nav.Link>
-
-              <Nav.Link as={NavLink} to={"/contacto"}>
-              {etiquetas[4]?.palabra}
-              </Nav.Link>
-
-              <Nav.Link as={NavLink} to={"/galeria"}>
-              {etiquetas[5]?.palabra}
-              </Nav.Link>
-            </Nav>
-
-            <NavIcons />
-
-          </Navbar.Collapse>
-        </div>
-
-      </Navbar> */}
-
-    
-
+      <div className={color ? "navbar navbar-bcg" : "navbar"}>
         <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className="nav-item">
-            <NavLink
-              to={"/"}
-              onClick={() => setClick(false)}
-            >
-               {etiquetas[0]?.palabra}
+            <NavLink to={"/"} onClick={() => setClick(false)}>
+              {etiquetas[0]?.palabra}
             </NavLink>
           </li>
 
           <li className="nav-item" onClick={() => setOpen(!open)}>
-            <NavLink  to={"/servicios"}>
-            {etiquetas[1]?.palabra}
-            </NavLink>
-
-            
+            <NavLink to={"/servicios"}>{etiquetas[1]?.palabra}</NavLink>
           </li>
           <li className="nav-item" onClick={() => setOpen(!open)}>
-            <NavLink  to={"/blog"}>
-            {etiquetas[2]?.palabra}
-            </NavLink>
-
-            
+            <NavLink to={"/blog"}>{etiquetas[2]?.palabra}</NavLink>
           </li>
           <div>
-          <Link to={"/"}>
-            <img  className={color ? "logo-sm" : "logo"} src={logo} alt="logo" />
-          </Link>
-        </div>
+            <Link to={"/"}>
+              <img
+                className={color ? "logo-sm" : "logo"}
+                src={logo}
+                alt="logo"
+              />
+            </Link>
+          </div>
 
           <li className="nav-item">
-            <NavLink
-              
-              to={"/nosotros"}
-              onClick={() => setClick(false)}
-            >
+            <NavLink to={"/nosotros"} onClick={() => setClick(false)}>
               {etiquetas[3]?.palabra}
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink
-              to={"/contacto"}
-              onClick={() => setClick(false)}
-            >
-                            {etiquetas[4]?.palabra}
-
+            <NavLink to={"/contacto"} onClick={() => setClick(false)}>
+              {etiquetas[4]?.palabra}
             </NavLink>
           </li>
           <li className="nav-item">
-            <NavLink
-              to={"/galeria"}
-              onClick={() => setClick(false)}
-            >
-                            {etiquetas[5]?.palabra}
-
+            <NavLink to={"/galeria"} onClick={() => setClick(false)}>
+              {etiquetas[5]?.palabra}
             </NavLink>
           </li>
           <li>
-            <NavIcons/>
+            <NavIcons />
           </li>
         </ul>
 
@@ -180,7 +92,9 @@ const Navmenu2 = () => {
             <FaBars size={30} style={{ color: "#002569" }} />
           )}
         </div>
-      
+      </div>
+
+     
     </>
   );
 };
